66268465617850894598a3e8ae63018bbce69d47
Ian-Wanjira <ianwanjira4@gmail.com>
2024-12-11 05:59:13 -0600

    feat: Add navigation links constants
    - Created constants/index.ts to define navigation links.
    - Added NAV_LINKS array containing labels and hrefs for the navigation menu: Home, Personal, Business, and FAQs.
    
    This addition centralizes the navigation link data, making it easier to manage and update the site's menu.

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Personal',
+    href: '#personal',
+  },
+  {
+    label: 'Business',
+    href: '#business',
+  },
+  {
+    label: 'FAQs',
+    href: '#faqs',
+  },
+];
